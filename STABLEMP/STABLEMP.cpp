71894c3badc05feab33b70781463db16027bf49b
oswald <pratyaksh@me.com>
2013-10-29 19:49:29 +0530

    AC STABLEMP

---
+++ STABLEMP/STABLEMP.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<queue>
+#include<list>
+using namespace std;
+int main(){
+	int t, tmp;
+	scanf("%i", &t);
+	while(t-->0){
+		int n;
+		scanf("%i ", &n);
+		int women[n][n], men[n][n];
+		for(int i=0; i<n; i++){
+			scanf("%i ", &tmp);
+			for(int j=0; j<n; j++){
+				scanf("%i ", &tmp);
+				women[i][tmp-1]=j;
+			}
+		 }
+		for(int i=0; i<n; i++){
+			scanf("%i", &tmp);
+			for(int j=0; j<n; j++){
+				scanf("%i ", &tmp);
+				men[i][j]=tmp-1;
+			}
+		} 
+		list<int> freemen;
+		for(int i=0; i<n; i++)
+			freemen.push_back(i);
+
+		int menListIndex[n], womenPartner[n];
+		for(int i=0; i<n; i++)
+			menListIndex[i]=womenPartner[i]=-1;
+		while(freemen.empty()==false){
+			int curr=freemen.front(), favWomanIndex=++menListIndex[curr];
+			int favWoman=men[curr][favWomanIndex];
+			if(womenPartner[men[curr][favWomanIndex]]==-1){
+				womenPartner[favWoman]=curr;
+				freemen.pop_front();
+			}
+			else{
+				if(women[favWoman][curr]<women[favWoman][womenPartner[favWoman]]){
+					freemen.push_back(womenPartner[favWoman]);
+					freemen.pop_front();
+					womenPartner[favWoman]=curr;
+				}
+			}
+		}
+		int ans[n];
+		for(int i=0; i<n; i++){
+			ans[womenPartner[i]]=i;
+		}
+		for(int i=0; i<n; i++){
+			printf("%i %i\n", i+1, ans[i]+1);
+		}
+
+
+	}
+}
+
