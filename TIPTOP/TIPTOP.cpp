03e52466f7da2479640995f023b98726e7e0fb92
oswald <pratyaksh@me.com>
2014-01-14 15:46:27 +0530

    AC TIPTOP

--- TIPTOP/TIPTOP.cpp
+++ TIPTOP/TIPTOP.cpp
@@ -11,7 +11,7 @@ int main()
 		bignum n;
 		scanf("%llu", &n);
 		bignum s=sqrt(n);
-		if(s*s==n)
+		if(s*s==n || (s-1)*(s-1)==n || (s+1)*(s+1)==n)
 			printf("Case %i: Yes\n", i+1);
 		else
 			printf("Case %i: No\n", i+1);
