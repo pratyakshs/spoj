216f93c67b9b3ad349c4c54b29fc295ce657b964
Pratyaksh Sharma <pratyaksh@me.com>
2015-07-12 17:58:24 +0200

    AC ONP

--- ONP/ONP.cpp
+++ ONP/ONP.cpp
@@ -1,82 +1,35 @@
-#include<stdio.h>
-#include<stack>
-#include<map>
-#include<iostream>
-#include<string>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-int main()
-{
-	map<char, int> m;
-	m['+']=1; 
-	m['-']=2;
-	m['*']=3;
-	m['/']=4;
-	m['^']=5;
-	m[')']=-1;
-	m['(']=-1;
+int main() {
+	ios_base::sync_with_stdio(0);
 	int t;
-	scanf("%i\n", &t);
-	stack<char> s;
-	char current;
-	for(int i=0; i<t; i++)
-	{
-		string str;
-		cin>>str;
-		int k=0;
-		while(1)
-		{
-			cout<<"here1"<<endl;
-			current=str[k];
-			cout<<"here2"<<endl;
-			if(m[current]==0)
-			{
-				printf("%c", current);
+	string s;
+	cin >> t;
+	while(t--) {
+		cin >> s;
+		stack<char> st;
+		for(int i = 0; i < s.length(); i++) {
+			if (s[i] == '(')
+				continue;
+			else if (s[i] == '*' || s[i] == '+' || s[i] == '-' || 
+					 s[i] == '/' || s[i] == '^')
+				st.push(s[i]);
+			else if (s[i] == ')') {
+				cout << st.top();
+				st.pop();
 			}
-			else
-			{
-				if(m[current]!=-1)
-				{
-					while(!s.empty())
-					{
-						if((current!='^' || m[current]<m[s.top()]))
-						{
-							printf("%c", s.top());
-							s.pop();
-						}
-					}
-					s.push(current);
-				}
-				else if(current=='(')
-				{
-					s.push('(');
-				}
-				else if(current==')')
-				{
-					while(s.top()!='(')
-					{
-						printf("%c", s.top());
-						s.pop();
-					}
-					s.pop();
-
-				}
-
-			};
-			while(!s.empty())
-			{
-				printf("%c", s.top());
-				s.pop();
-			}
-			k++;
-			if(k==str.length())
-			{
-				cout<<"fin";	
-				break;
-			}	
-
+			else cout << s[i];
 		}
-		printf("\n");
+		cout << endl;
 	}
+	return 0;
 }
+
+/**
+ * Straightforward application of stack. 
+ * On operator: push
+ * On variable: print
+ * On '(': nothing
+ * On ')': pop and print
+ */
